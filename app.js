c130cb3978037664300f3952b6e0db8d206019b7
Sylvesterel <sylvester@erbsledet.dk>
2025-12-03 10:17:20 +0100

    express

---
+++ app.js
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv') // Til at lave en .env fil, så vi kan have lokale variabler.
+const mysql = require('mysql2/promise') // Til at importere data fra fra en MySQL database. Vi bruger promise for at kunne udnytte asynkronitet til flere MySQL kald.
+const path = require('path') // Path som indbygget node.js modul, så vi kan lave en statisk public mappe til HTML filer.
+const express = require('express')
+
+dotenv.config(); // configure vores .env fil
+
+const app = express();
+const port = 8080;
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+/*  -----------
+
+    Forklaring af Express.static og path.join(process.cwd)
+     1. Kombinationen er path.join og process.cwd returnere hvilken mappe som vores fil/express server kører fra.
+     1. Kombinationen er path.join og process.cwd returnere hvilken mappe som vores fil/express app kører fra.
+     2. express.static laver et middelware hvor filer er tilgængelige igennem HTTP.
+     I dette tilfælde bruger vi mappen "public", som vores offentlig tilgængelige mappe via HTTP.
+
+    ----------- */
+
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.get("/data", async (req, res) => {
+    const update = await pool.execute(`SELECT * FROM data`)
+    res.send(update)
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
