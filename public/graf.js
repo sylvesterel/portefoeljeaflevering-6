143c3d078c34f47f087ed53e28c043b91817a100
nola0002 <nola0002@stud.ek.dk>
2025-12-09 12:31:30 +0100

    Pushet første graf som har fået dataen ind færdig

--- public/graf.js
+++ public/graf.js
@@ -1,50 +1,64 @@
-async function printData() {
-    const url = "/data/chart1"
-    const response = await fetch(url, {
+async function fetchData(endpoint) {
+
+    const response = await fetch(endpoint, {
         method: 'GET',
         headers: {
             'Accept': 'application/json'
         }
     })
-    console.log(await response.json())
+    const data = await response.json()
+    return data
 }
 
-printData();
+let yearArray = []
+let employedWomenArray = []
 
-const ctx = document.querySelector('#chart1').getContext('2d');
-const chart1 = new Chart(ctx, {
-    type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-    data: {
-        labels: ['monday', 'tuesday', 'wednesday', 'thursday'],
-        datasets: [{
-            label: 'Tiktok views',
-            data: [24, 75, 87, 70],
-            backgroundColor:'#D8D8D8'
-        },
-            {
-                label: 'Youtube views',
-                data: [67, 69, 57, 80],
-                backgroundColor:'blue',
-                borderColor: 'blue'
+async function pushDataToArrayChart1 () {
+    const data = await fetchData('/data/chart1')
+
+    data.forEach(year => {
+        yearArray.push(year.year)
+        employedWomenArray.push(year.employed_women)
+    })
+}
+
+
+
+async function renderChart(){
+    await pushDataToArrayChart1()
+    console.log(yearArray)
+    console.log(employedWomenArray)
+    const ctx = document.querySelector('#chart1').getContext('2d');
+    const chart1 = new Chart(ctx, {
+        type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data: {
+            labels: yearArray,
+            datasets: [{
+                label: 'Kvinder ansat i IT-Branchen',
+                data: employedWomenArray,
+                backgroundColor:'#D8D8D8'
             }]
-    },
-    options: {
-        plugins: {
-            title: {
-                display: true,
-                text: 'TikTok vs Youtube views'
-            },
-            legend: {
-                display: true,
-                position: 'bottom'
-            }
         },
-        layout:{
-            padding: {
-                right: 800,
-                left: 50
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Kvinder ansat i IT-Branchen'
+                },
+                legend: {
+                    display: true,
+                    position: 'bottom'
+                }
+            },
+            layout:{
+                padding: {
+                    right: 800,
+                    left: 50
+                }
             }
         }
-    }
 
-});
+    });
+}
+
+renderChart()
