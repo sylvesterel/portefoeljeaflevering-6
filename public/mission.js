33fb96553350917eaccb119b7221b16753ee00f1
mathildefarnoe <mat_farnoe@hotmail.com>
2025-12-11 10:07:25 +0100

    Mission

---
+++ public/mission.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.wrapper', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    //Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
+
