2722bdc22f8fd5c49995ef6a56b269c7e53c8ec0
nola0002 <nola0002@stud.ek.dk>
2025-12-12 13:39:57 +0100

    Added fleksible job procenter direkte fra databasen

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -1,20 +1,50 @@
-let workplaceList = []
-let workplacePrCountryPart = {}
+const flexJobH3 = document.querySelector('#fleks-job')
+const nonFlexJobH3 = document.querySelector('#ikke-fleks-job')
 
-async function fetchDataToList() {
-    workplaceResponse = await fetch("http://localhost:8080/jobs/2019")
-    workplaceList = await workplaceResponse.json()
-    return true;
+
+async function fetchData(endpoint) {
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    const data = await response.json()
+    return data
 }
 
-async function countWorkplacePrCountryPart() {
-    await fetchDataToList();
-    for (const data of workplaceList) {
-        const countryPart = data.navn
-        workplacePrCountryPart[countryPart] = data["2019"]
 
-    }
-    console.log(workplacePrCountryPart)
+const ruleConditions = []
+const percentageOfConditions = []
+
+
+async function pushDataToArrayAndLogPercentagesAndRenderPercentages () {
+    const data = await fetchData('/data/workFreedom')
+
+    data.forEach(workRulesCondition => {
+        ruleConditions.push(workRulesCondition.rule_type)
+        percentageOfConditions.push(workRulesCondition.percentage)
+    })
+    const hybridJobsPercentage = percentageOfConditions[0]
+    const alwaysInOfficePercentage = percentageOfConditions[1]
+    const alwaysRemotePercentage = percentageOfConditions[2]
+    const noCompanyWideGuideLinesPercentage = percentageOfConditions[3]
+
+    const flexibleJobsPercentage = hybridJobsPercentage + alwaysRemotePercentage + noCompanyWideGuideLinesPercentage
+    console.log(flexibleJobsPercentage)
+
+
+    const nonFlexibleJobsPercentage = alwaysInOfficePercentage
+    console.log(nonFlexibleJobsPercentage)
+    console.log(ruleConditions)
+    console.log(percentageOfConditions)
+
+    flexJobH3.textContent = `${flexibleJobsPercentage}%`
+    nonFlexJobH3.textContent = `${nonFlexibleJobsPercentage}%`
 }
 
-countWorkplacePrCountryPart();
+pushDataToArrayAndLogPercentagesAndRenderPercentages()
+
+
+
+
