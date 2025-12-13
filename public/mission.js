10f780a5fc852ea55f7a712e73f19884acf6f902
johannejeils-ek <joje@stud.ek.dk>
2025-12-13 09:01:12 +0100

    Update Forside

--- public/mission.js
+++ public/mission.js
@@ -23,6 +23,8 @@ const swiper = new Swiper('.wrapper', {
     }
 });
 
+
+
 window.addEventListener("scroll", function() {
     const navbar = document.querySelector(".navbar");
     navbar.classList.toggle("scrolled", window.scrollY > 50);
